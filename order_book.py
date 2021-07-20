8cf4c8c666daf79036d4033d933a67e0c3e972fe
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-07-19 18:23:00 -0700

    try printing

--- order_book.py
+++ order_book.py
@@ -14,13 +14,17 @@ def process_order(order):
     # 1. Insert this order into the order book
     fields = ['buy_currency', 'sell_currency', 'buy_amount', 'sell_amount', 'sender_pk', 'receiver_pk']
     order_obj = Order(**{f: order[f] for f in fields})
+    print("Order imported.")
     session.add(order_obj)
     session.commit()
 
     # 2. Check if there are any existing orders that match
     orders = session.query(Order).filter(Order.filled is None).all()
+    print("Order retrieved.")
     for existing_oder in orders:
-        if existing_oder.buy_currency == order_obj.sell_currency and existing_oder.sell_currency == order_obj.buy_currency:
+        print(existing_oder.id)
+        if existing_oder.buy_currency == order_obj.sell_currency and \
+                existing_oder.sell_currency == order_obj.buy_currency:
             if existing_oder.sell_amount / existing_oder.buy_amount >= order_obj.buy_amount / order_obj.sell_amount:
                 # If a match is found
                 existing_oder.filled = datetime.now()
@@ -33,12 +37,20 @@ def process_order(order):
                     new_order_obj = Order(sender_pk=order['sender_pk'], receiver_pk=order['receiver_pk'],
                                           buy_currency=order['buy_currency'], sell_currency=order['sell_currency'],
                                           buy_amount=order['buy_amount'] - existing_oder['sell_amount'],
-                                          sell_amount=order['sell_amount'] - existing_oder['buy_amount'])
+                                          sell_amount=order['sell_amount'] - existing_oder['buy_amount'],
+                                          creator_id=order.id)
                     new_order_obj = Order(**{f: new_order_obj[f] for f in fields})
-                    session.add(new_order_obj)
-                    session.commit()
-                    process_order(new_order_obj)
-                
+                elif order_obj.sell_amount < existing_oder.buy_amount:
+                    new_order_obj = Order(sender_pk=existing_oder.sender_pk, receiver_pk=existing_oder.receiver_pk,
+                                          buy_currency=existing_oder.buy_currency, sell_currency=existing_oder.sell_currency,
+                                          buy_amount=existing_oder.buy_amount - order['sell_amount'],
+                                          sell_amount=existing_oder.sell_amount - order['buy_amount'],
+                                          creator_id=existing_oder.id)
+
+                new_order_obj = Order(**{f: new_order_obj[f] for f in fields})
+                session.add(new_order_obj)
+                session.commit()
+                process_order(new_order_obj)
 
     # 4. If there are multiple ways to fill an order
 
