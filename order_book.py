9d3e84f558a10c47acfabdfc097ee9d63793cbb7
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-07-19 19:23:32 -0700

    test

--- order_book.py
+++ order_book.py
@@ -22,36 +22,39 @@ def process_order(order):
     session.commit()
 
     # 2. Check if there are any existing orders that match
+    matched = False
     orders = [order for order in session.query(Order).filter(Order.filled == None).all()]
-    # print("Order retrieved. {}".format(len(orders)))
     for existing_oder in orders:
         if existing_oder.buy_currency == order_obj.sell_currency and \
                 existing_oder.sell_currency == order_obj.buy_currency:
             if existing_oder.sell_amount / existing_oder.buy_amount >= order_obj.buy_amount / order_obj.sell_amount:
                 # If a match is found
+                matched = True
                 existing_oder.filled = datetime.now()
                 order_obj.filled = datetime.now()
                 existing_oder.counterparty_id = order_obj.id
                 order_obj.counterparty_id = existing_oder.id
                 session.commit()
-                # If one of the orders is not completely filled
-                if existing_oder.sell_amount < order_obj.buy_amount:
-                    new_order_obj = Order(sender_pk=order_obj.sender_pk, receiver_pk=order_obj.receiver_pk,
-                                          buy_currency=order_obj.buy_currency, sell_currency=order_obj.sell_currency,
-                                          buy_amount=order_obj.buy_amount - existing_oder.sell_amount,
-                                          sell_amount=order_obj.sell_amount - existing_oder.buy_amount,
-                                          creator_id=order_obj.id)
+                break
 
-                elif order_obj.sell_amount < existing_oder.buy_amount:
-                    new_order_obj = Order(sender_pk=existing_oder.sender_pk, receiver_pk=existing_oder.receiver_pk,
-                                          buy_currency=existing_oder.buy_currency,
-                                          sell_currency=existing_oder.sell_currency,
-                                          buy_amount=existing_oder.buy_amount - order_obj.sell_amount,
-                                          sell_amount=existing_oder.sell_amount - order_obj.buy_amount,
-                                          creator_id=existing_oder.id)
-                else:
-                    break
+    if matched:
+        # If one of the orders is not completely filled
+        if existing_oder.sell_amount < order_obj.buy_amount:
+            new_order_obj = Order(sender_pk=order_obj.sender_pk, receiver_pk=order_obj.receiver_pk,
+                                  buy_currency=order_obj.buy_currency, sell_currency=order_obj.sell_currency,
+                                  buy_amount=order_obj.buy_amount - existing_oder.sell_amount,
+                                  sell_amount=order_obj.sell_amount - existing_oder.buy_amount,
+                                  creator_id=order_obj.id)
 
-    session.add(new_order_obj)
-    session.commit()
-    process_order(new_order_obj)
+        elif order_obj.sell_amount < existing_oder.buy_amount:
+            new_order_obj = Order(sender_pk=existing_oder.sender_pk, receiver_pk=existing_oder.receiver_pk,
+                                  buy_currency=existing_oder.buy_currency,
+                                  sell_currency=existing_oder.sell_currency,
+                                  buy_amount=existing_oder.buy_amount - order_obj.sell_amount,
+                                  sell_amount=existing_oder.sell_amount - order_obj.buy_amount,
+                                  creator_id=existing_oder.id)
+        else:
+            pass
+        session.add(new_order_obj)
+        session.commit()
+        process_order(new_order_obj)
